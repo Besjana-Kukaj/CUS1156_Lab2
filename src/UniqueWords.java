bd33c2df818a3e302fcc127aa68daba121a8ed0e
Besjana-Kukaj <beskukaj2004@gmail.com>
2024-09-30 21:23:44 -0400

    Completed contUnique method!

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -1,37 +1,45 @@
 import java.util.ArrayList;
 
-public class UniqueWords
-{
-   /**
-		counts the number of unique strings in a list
-		@param list ArrayList of strings to be examined
-		@return number of unique strings in the list
-   */
-   public static int countUnique(ArrayList<String> list)
-   {
-	  int count = 0;
-	  
-      for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
-		 }
-      }
-	  return count;
-   }
+public class UniqueWords {
+	/**
+	 * counts the number of unique strings in a list
+	 * 
+	 * @param list ArrayList of strings to be examined
+	 * @return number of unique strings in the list
+	 */
+	public static int countUnique(ArrayList<String> list) {
 
-   public static void main(String[] args)
-   {
-      ArrayList <String> words = new ArrayList<>();
-      words.add("apple");
-	  words.add("orange");
-	  words.add("blackboard");
-	  words.add("apple");
-	  words.add("orange");
-	  words.add("sun");
-	  words.add("moon");
-   
-	  int unique = countUnique(words);
-      System.out.println(words + " has " + unique + " unique words");
-   }
+		int count = 0;
+
+		for (int i = 0; i < list.size(); i++) { 
+			boolean unique = true;
+			for (int j = 0; j < list.size(); j++) {
+				if (i != j && list.get(j).equals(list.get(i))) {// unique is true unless these parameters proving a duplicate is passed
+					unique = false;
+				}
+
+			}
+			if (unique) { //If the boolean returns true, the counter will raise by one!!
+				count++;
+			}
+		}
+
+		return count;
+
+	}
+
+	public static void main(String[] args) {
+		ArrayList<String> words = new ArrayList<>();
+		words.add("apple");
+		words.add("orange");
+		words.add("blackboard");
+		words.add("apple");
+		words.add("orange");
+		words.add("sun");
+		words.add("moon");
+		
+
+		int unique = countUnique(words);
+		System.out.println(words + " has " + unique + " unique words");
+	}
 }
